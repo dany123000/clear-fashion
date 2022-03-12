77cb3726b074ee036414efa0251e599e0592059e
dany123000 <83404634+dany123000@users.noreply.github.com>
2022-03-12 10:55:29 +0100

    Update api.js

--- server/api.js
+++ server/api.js
@@ -15,10 +15,16 @@ app.use(helmet());
 
 app.options('*', cors());
 
+app.get('/products/search', (request, response) => {
+  db.find({'_id':'678384c9-20bd-5151-9e1a-382984501eb8'}).then(x => {
+    response.send(request.query);
+  });
+});
+
 app.get('/products/:id', (request, response) => {
   db.find({'_id':request.params.id}).then(x => {
     response.send({'product': x});
-  }  );
+  });
 });
 
 app.listen(PORT);
